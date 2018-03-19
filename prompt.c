4752f5b4869e24c838c4573dbd4379bea6c2cfbd
maupinpamela <maupinpamela@gmail.com>
2018-03-19 21:38:42 +0000

    Working file needs putchar.c exec.c

--- prompt.c
+++ prompt.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 
-int main(__attribute__((unused)) int ac, char **av)
+int main(__attribute__((unused)) int argc, char **argv)
 {
 	int words = 0;
 	size_t buffersize = 1024, space = 0;
@@ -11,28 +11,24 @@ int main(__attribute__((unused)) int ac, char **av)
 	{
 		return (0);
 	}
-	if (av[words] == '\0')
+	if (argv[words] == '\0')
 	{
 		perror("ERROR TRY AGAIN");
 		words = 0;
 		exit(98);
 		_putchar('\n');
 	}
-	while (av[words] != NULL)
+	while (argv[words] != NULL)
 	{
 		_putchar('$');
 		_putchar(' ');
 		while (getline (&buffer, &buffersize, stdin) != -1)
 		{
 			write(1, buffer, space);
+			execute_cmd();
 			_putchar('$');
 			_putchar(' ');
 			words++;
-		}
-		while (getline (&buffer, &buffersize, stdin) == -1)
-		{
-			perror("TRY AGAIN no data");
-			printf ("98");
 		}
 	}
 	free(buffer);
