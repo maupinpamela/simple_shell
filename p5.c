736d7c1b3e33a3ecf3c4177aa5e77084eb00c84f
maupinpamela <maupinpamela@gmail.com>
2018-03-23 17:01:58 +0000

    fork

---
+++ p5.c
@@ -0,0 +1,21 @@
+#include "p.h"
+
+int forks(char **args)
+{
+	pid_t getp;
+	int status;
+
+	getp = fork();
+	if (getp == 0)
+	{
+		printf("%s\n", args[0]);
+		execve(args[0], args, NULL);
+
+		if (getp < 0)
+			perror ("no child");
+
+		else
+			wait(&status);
+	}
+	return (1);
+}
