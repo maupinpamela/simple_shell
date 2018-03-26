4d986c21c1bc1dbbc21b59880e21536d78f5ed0c
maupinpamela <maupinpamela@gmail.com>
2018-03-26 03:06:33 +0000

    String token

--- p4.c
+++ p4.c
@@ -8,23 +8,38 @@ char **parseline(char *line)
 	int buffersize = max_length;
 
 	copy = malloc(sizeof(char*) * buffersize);
+	if (copy == NULL)
+		return (NULL);
 
 	token = strtok(line, delims);
 
+	look_line(token);
+
 	if (token == NULL)
-		return (0);
+		return (NULL);
+
 	copy[i] = token;
 	i++;
 
 	if (token == NULL)
 		perror ("Can't allocate");
-
 	while (token != NULL)
 	{
 		token = strtok(NULL, delims);
 		copy[i] = token;
 		i++;
 	}
-
 	return (copy);
+}
+
+char *look_line(char *token)
+{
+
+	if (_strcmp(token, "exit") == 0)
+ 		 exit(1);
+
+	if(_strcmp(token, "env") == 0)
+		print_env();
+
+	return (token);
 }
