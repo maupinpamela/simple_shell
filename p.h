2cd016635f95089f6901ef8d2d1e2f1f97326a1f
maupinpamela <maupinpamela@gmail.com>
2018-03-26 03:09:13 +0000

    header file

--- p.h
+++ p.h
@@ -4,33 +4,28 @@
 #include <stdio.h>
 #include <string.h>
 
+/*Defined Macros*/
 #define max_length 1024;
 /*#define delims " \t\r\n\a"*/
 
+extern char **environ;
+
+/*Main Functions*/
 void beginning(void);
 char *takeline(void);
 char **parseline(char *line);
 int forks(char **args);
 int exline(char **args);
-int exit_fun(char **args);
-/*
-  Function Declarations for builtin shell commands:
 
-char *bi_str[] = {
-	"exit"
-};
+/*Helper Functions*/
 
-
-int (*bi[]) (char **) = {
-	&exit_fun
-};
-
-int bi_return() {
-	return sizeof(bi_str) / sizeof(char *);
-}
-
-int exit_fun(char **args)
-{
-	return 0;
-}
-*/
+int print_env(void);
+int _putchar(char c);
+char *_strdup(char *str);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char errors_line(char line);
+char *_getenv(char *name);
+char *look_line(char *token);
