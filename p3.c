2f896f42789e8a653f643f21be4d72706b993642
maupinpamela <maupinpamela@gmail.com>
2018-03-26 03:05:43 +0000

    Getline function

--- p3.c
+++ p3.c
@@ -4,7 +4,21 @@ char *takeline(void)
 {
 	size_t buffersize = 0;
 	char *line;
+	int check = 0;
 
-	getline(&line, &buffersize, stdin);
+	check = getline(&line, &buffersize, stdin);
+
+	if (check == EOF)
+	{
+		_putchar('\n');
+		free(line);
+		exit (EXIT_SUCCESS);;
+	}
+
+	if (line == NULL)
+	{
+		return (NULL);
+	}
+
 	return (line);
 }
