505043371d5a633ace90d8c52c9a891167878691
maupinpamela <maupinpamela@gmail.com>
2018-03-27 17:35:04 +0000

    Betty fixes

---
+++ getline1.c
@@ -0,0 +1,27 @@
+#include "p.h"
+/**
+ *takeline- gets the input from the command line checks to see if its EOF
+ * Return: returns the line pointer to the next function
+ */
+char *takeline(void)
+{
+	size_t buffersize = 0;
+	char *line;
+	int check = 0;
+
+	check = getline(&line, &buffersize, stdin);/*gets input from command line*/
+
+	if (check == EOF)/*checks to see if the input is EOF*/
+	{
+		_putchar('\n');
+		free(line);
+		exit(EXIT_SUCCESS);
+	}
+
+	if (line == NULL)/*if line is empty it will return NULL*/
+	{
+		return (NULL);
+	}
+
+	return (line);
+}
