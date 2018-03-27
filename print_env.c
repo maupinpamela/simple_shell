bf5d35ccd3f2003b87564e13f042e27164ce0ad4
maupinpamela <maupinpamela@gmail.com>
2018-03-27 17:44:35 +0000

    Betty fixes

--- print_env.c
+++ print_env.c
@@ -1,10 +1,13 @@
 #include "p.h"
-
+/**
+ *print_env- writes the env
+ * Return: nothing
+ */
 int print_env(void)
 {
 	int command = 0;
 
-	while(environ[command] != NULL)
+	while (environ[command] != NULL)
 	{
 		write(STDOUT_FILENO, environ[command], _strlen(environ[command]));
 		_putchar('\n');
