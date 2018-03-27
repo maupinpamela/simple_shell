0a3c75264a6cd93b8150928e091da6982d6addf1
maupinpamela <maupinpamela@gmail.com>
2018-03-27 04:56:10 +0000

    prints the PATH

---
+++ print_path.c
@@ -0,0 +1,37 @@
+#include "p.h"
+
+char **print_path(char *cmd)
+{
+	char *token = NULL, *path = NULL, *ret = 0, *hold = NULL;
+	char **newstr = NULL;
+	char *delim = "=:\n";
+	int counter = 0, colon = 0, j = 0;
+
+	path = (_getenv("PATH"));
+
+	while (path[colon])
+	{
+		if (path == delim)
+			counter++;
+	}
+
+	token = malloc(sizeof(counter + 1) * (*token));
+	if (token == NULL)
+		return (NULL);
+
+	token = strtok(cmd, delim);
+	if (token == NULL)
+		return (NULL);
+	colon = 0;
+
+	while (counter > colon)
+	{
+		ret = str_concat(hold, "/");
+		ret = str_concat(hold, cmd);
+		newstr[j] = ret;
+		ret = strtok(NULL, delim);
+	}
+	free (path);
+	newstr = '\0';
+	return (newstr);
+}
