f0bb255aed50b0305cd39701bfad0b9cf727aaea
tragic86 <oneil5@aol.com>
2018-03-28 04:32:38 +0000

    linked list function make and check

--- print_path.c
+++ print_path.c
@@ -4,38 +4,63 @@
  * @cmd: the command that was entered in the input line
  * Return: a concatinated string
  */
-char **print_path(char *cmd)
+path_t *make_path(void)
 {
-	char *token = NULL, *path = NULL, *ret = 0, *hold = NULL;
-	char **newstr = NULL;
-	char *delim = "=:\n";
-	int counter = 0, colon = 0, j = 0;
+	char *token = NULL, *path = NULL;
+	path_t *head, *temp;
 
-	path = (_getenv("PATH"));
-
-	while (path[colon])
-	{
-		if (path == delim)
-			counter++;
-	}
-
-	token = malloc(sizeof(counter + 1) * (*token));
+	path = _strdup(_getenv("PATH"));
+	if (path == NULL)
+		return (NULL);
+	token = strtok(path , ":");
 	if (token == NULL)
 		return (NULL);
 
-	token = strtok(cmd, delim);
-	if (token == NULL)
+	temp = malloc(sizeof(path_t));
+	if (temp == NULL)
 		return (NULL);
-	colon = 0;
-
-	while (counter > colon)
+	head = temp;
+	/*loop*/
+	while (token != NULL)
 	{
-		ret = str_concat(hold, "/");
-		ret = str_concat(hold, cmd);
-		newstr[j] = ret;
-		ret = strtok(NULL, delim);
+		temp->s = _strdup(token);
+		if (temp->s == NULL)/*copt dir*/
+			return (NULL);
+		token = strtok(NULL, ":");
+		if (token != NULL)
+		{
+			temp->next = (malloc(sizeof(path_t)));/*malloc only if we have another token*/
+			temp = temp->next;/*move to the next node*/
+		}
+		else
+			temp->next = NULL;
+		
 	}
-	free(path);
-	newstr = '\0';
-	return (newstr);
+	free (path);
+	return (head);
+
+}
+
+
+
+char *check_path(path_t *head, char *cmd)
+{
+	/*concats*/
+	path_t *temp;
+	struct stat st;
+	char *copy;
+
+	temp = head;
+	while (temp)
+	{
+		copy = _strcat(temp->s, "/");
+		copy = _strcat(copy, cmd);
+		
+		if (stat(copy, &st) == 0)
+			return (copy);
+		else
+			free(copy);
+		temp = temp->next;
+	}
+	return (NULL);
 }
