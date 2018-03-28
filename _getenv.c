0d06ad7d317503ae0b4368ab7db1fa259ebd03a4
tragic86 <oneil5@aol.com>
2018-03-28 04:32:12 +0000

    linked list add

---
+++ _getenv.c
@@ -0,0 +1,46 @@
+#include "p.h"
+/**
+ *
+ *
+ *
+ */
+
+
+
+char *_getenv(char *name)
+{
+
+	int i, j;
+	char *match = NULL;
+	char *res;
+
+
+	i = 0;
+
+	if (name == NULL)
+	{
+		return (NULL);
+	}
+
+
+
+	while (environ[i] != NULL)
+	{
+		j = 0;
+		while (name[j] != '\0')
+		{
+			if (name[j] != environ[i][j])
+				break;
+			if (name[j + 1] == '\0' && environ[i][j + 1] == '=')
+				match = _strdup(environ[i]);
+			j++;
+		}
+		if (match)
+			break;
+		i++;
+	}
+	strtok(match, "=");
+	res = strtok(NULL, "=");
+	return (res);
+}
+
