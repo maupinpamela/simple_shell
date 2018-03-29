bb06751e6a9104f6d286faa570bcf7c5b627fe2a
maupinpamela <maupinpamela@gmail.com>
2018-03-29 06:17:43 +0000

    added concat prototype

--- p.h
+++ p.h
@@ -53,5 +53,6 @@ char *look_line(char *token);
 int _strncmp(const char *s1, const char *s2, size_t n);
 char *str_concat(char *s1, char *s2);
 void free_list(path_t *head);
-
+int is_space(char *line);
+char *str_concat(char *s1, char *s2);
 #endif
