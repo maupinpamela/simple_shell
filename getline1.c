76d15aa92a9bb41f912629bb4e55100545a562a6
maupinpamela <maupinpamela@gmail.com>
2018-03-29 06:16:33 +0000

    added frees

--- getline1.c
+++ getline1.c
@@ -6,18 +6,24 @@
 char *takeline(void)
 {
 	size_t buffersize = 0;
-	char *line;
+	char *line = NULL, *ex = "exit";
 	int check;
 
 
 
 	check = getline(&line, &buffersize, stdin);/*gets input from command line*/
+	if (check == 0)
+		free(line);
 
 	if (check == EOF)/*checks to see if the input is EOF*/
 	{
-		/*_putchar('\n');*/
 		free(line);
 		exit(EXIT_SUCCESS);
+	}
+	if (_strcmp(line, ex) == 0)
+	{
+		free(line);
+		exit(0);
 	}
 	if (line == 0)/*if line is empty it will return NULL*/
 	{
